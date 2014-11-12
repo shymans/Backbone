3dbf181672c06ef0d2588dfd3a89b236b2feb581
Austin Shymanski <austin.shymanski@gmail.com>
2014-11-12 12:25:03 -0500

    created simple server connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 var port = 3000;
 
 connect.createServer(
-  connect.static(__dirname);
+  connect.static(__dirname)
 ).listen(port);
 
 console.log('You are connected at port ' + port);
